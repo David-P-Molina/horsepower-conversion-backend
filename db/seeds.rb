fa77700f5a24f4a0fc246aea45794096729014b0
David Moina <davemolina777@gmail.com>
2021-06-09 13:08:36 -0500

    Adding seed data for standard measurement

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+foot = StandardizedMeasurement.new(name: "Foot", type: "Length", unit_of_measurement: 12)
+inch = StandardizedMeasurement.new(name: "Inch", type: "Length", unit_of_measurement: 1)
+horsepower = StandardizedMeasurement.new(name: "Horse-Power", type: "Energy", unit_of_measurement: 1)
