7f73a8688aad2bf39306dd7af5e0d4e1eccc5130
David Moina <davemolina777@gmail.com>
2021-06-09 14:19:58 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :conversions
+  resources :measurements
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
