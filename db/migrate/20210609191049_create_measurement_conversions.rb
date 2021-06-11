94b4ac0adc5e34d46a85b862368e53ab70899991
David Moina <davemolina777@gmail.com>
2021-06-11 12:32:58 -0500

    Added column name to conversion

--- db/migrate/20210609191049_create_measurement_conversions.rb
+++ db/migrate/20210609191049_create_measurement_conversions.rb
@@ -3,7 +3,7 @@ class CreateMeasurementConversions < ActiveRecord::Migration[6.1]
     create_table :measurement_conversions do |t|
       t.references :measurement, null: false, foreign_key: {on_delete: :cascade}
       t.references :conversion, null: false, foreign_key: {on_delete: :cascade}
-
+      t.string :name
       t.timestamps
     end
   end
