9fe6f7b2e6a74f7806e2955dc0f25159fa85b241
David Moina <davemolina777@gmail.com>
2021-06-15 14:12:45 -0500

    Added getter setter helper methods for username

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -7,4 +7,12 @@ class Conversion < ApplicationRecord
     #if measurement is standard do nothing
     #method that takes hp value and converts quantity 2 into correct number
     #method that assigns the value to measurement_2_quantity
+
+    #helper methods
+    def username=(username)
+        self.username = User.find_or_create_by(username: username)
+    end
+    def username 
+        self.user.try(:username)
+    end
 end
