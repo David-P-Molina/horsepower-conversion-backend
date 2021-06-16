fe60545eab0105328d8252469065c44e1248c9db
David Moina <davemolina777@gmail.com>
2021-06-16 13:31:51 -0500

    Added strong params helper to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,4 +40,7 @@ class UsersController < ApplicationController
         def set_user
             @user = User.find(params[:id])
         end
+        def user_params
+            params.require(:users).permit(:username)
+        end
 end
