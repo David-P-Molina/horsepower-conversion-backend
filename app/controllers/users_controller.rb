4dc735693eeab7e261ef5267be9046bc677d207a
David Moina <davemolina777@gmail.com>
2021-06-16 13:25:07 -0500

    Created index action and filtered information provided

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show]
-  
+    
+    #GET /users
+    def index
+        @users = User.all
+        render json: @users, except: [:updated_at, :created_at]
+    end
     private
       # Use callbacks to share common setup or constraints between actions.
         def set_user
