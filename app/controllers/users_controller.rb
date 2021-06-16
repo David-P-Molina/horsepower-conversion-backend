9d69eebec907c2e2fe04a6dd8ee13711a282579f
David Moina <davemolina777@gmail.com>
2021-06-16 13:23:39 -0500

    Created helper method to set user for show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show]
   
+    private
+      # Use callbacks to share common setup or constraints between actions.
+        def set_user
+            @user = User.find(params[:id])
+        end
 end
