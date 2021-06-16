7f8578cc437dc55914fe9834b1c2fd61139db9ef
David Moina <davemolina777@gmail.com>
2021-06-16 13:29:04 -0500

    Added create, update, delete actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,30 @@ class UsersController < ApplicationController
     def show
         render json: @user.slice(:id, :username)
     end
-    
+     # POST /users
+    # def create
+    #   @user = User.new(user_params)
+  
+    #   if @user.save
+    #     render json: @user, status: :created, location: @user
+    #   else
+    #     render json: @user.errors, status: :unprocessable_entity
+    #   end
+    # end
+  
+    # PATCH/PUT /users/1
+    # def update
+    #   if @user.update(user_params)
+    #     render json: @user
+    #   else
+    #     render json: @user.errors, status: :unprocessable_entity
+    #   end
+    # end
+  
+    # DELETE /users/1
+    # def destroy
+    #   @user.destroy
+    # end
     private
       # Use callbacks to share common setup or constraints between actions.
         def set_user
