a862f69b95b441bc1a2228d0584a13d5f4c009e3
David Moina <davemolina777@gmail.com>
2021-06-16 13:39:28 -0500

    narrowed down routes used for backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :conversions
-  resources :measurements
-  resources :users
+  resources :conversions, except: [:update, :delete]
+  resources :measurements, only: [:index, :show]
+  resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
