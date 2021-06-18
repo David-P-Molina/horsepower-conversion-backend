841abcdc06ec5f39e9923b84b779911b1eb47f22
David Moina <davemolina777@gmail.com>
2021-06-18 11:48:46 -0500

    updated strong params and show action information

--- db/migrate/20210609190616_create_conversions.rb
+++ db/migrate/20210609190616_create_conversions.rb
@@ -2,8 +2,8 @@ class CreateConversions < ActiveRecord::Migration[6.1]
   def change
     create_table :conversions do |t|
       t.string :name
-      t.float :measure_1_quantity
-      t.float :measure_2_quantity
+      t.float :hp_quantity
+      t.float :animal_conversion_quantity
       t.references :user, foreign_key: {on_delete: :cascade}
 
       t.timestamps
